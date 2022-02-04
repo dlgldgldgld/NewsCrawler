d3d391d486a8bc49d38adf1b2311d6d174b46711
dlgldgld <shin12272014@gmail.com>
2022-02-04 18:57:55 +0900

    adding result directory.

--- newscrawler.py
+++ newscrawler.py
@@ -4,6 +4,7 @@ from crawler.iCrawler import iCrawler
 from crawler.NaverCrawler import NaverCrawler
 import time
 import csv
+import sys
 
 class newscrawler( ) :
     """
@@ -30,7 +31,11 @@ if __name__ == "__main__" :
         exit(0)
 
     # csv file reader
-    filename = time.strftime('%Y%m%d_%H%M%S') + str('.csv')
+    outfile_path = ''
+    if len(sys.argv) > 1 :
+        outfile_path = sys.argv[1] + '/'
+
+    filename = outfile_path + time.strftime('%Y%m%d_%H%M%S') + str('.csv')
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile : 
         fieldnames = newsitems[0].getRecByDict().keys()
         writer = csv.DictWriter(csvfile, fieldnames = fieldnames )
