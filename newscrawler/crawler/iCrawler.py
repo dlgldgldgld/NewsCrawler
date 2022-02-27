68ef6696bd8f23d347f0d21ef928c0b7dea9bee9
dlgldgld <shin12272014@gmail.com>
2022-02-27 21:37:15 +0900

    adapting pep8 styleguide

--- newscrawler/crawler/iCrawler.py
+++ newscrawler/crawler/iCrawler.py
@@ -1,30 +1,32 @@
 import abc
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-class iCrawler( metaclass = abc.ABCMeta ) :
+
+class iCrawler(metaclass=abc.ABCMeta):
     """
-        interface for defining crawler.
-        initilize common variable and define essential methods.
+    interface for defining crawler.
+    initilize common variable and define essential methods.
     """
+
     @property
-    def urlpath( self ) :
+    def urlpath(self):
         return self._urlpath
 
     @urlpath.setter
-    def urlpath( self, urlpath ) :
+    def urlpath(self, urlpath):
         self._urlpath = urlpath
 
     @abc.abstractmethod
-    def getNewsItem(self, url) -> list :
+    def getnewsitem(self, url) -> list:
         pass
 
-    def Exec(self) -> list :
+    def exec(self) -> list:
         url_lists = self._urlpath
         category_item = list()
 
         with ProcessPoolExecutor() as e:
-            futures = [e.submit(self.getNewsItem, url) for url in url_lists]
+            futures = [e.submit(self.getnewsitem, url) for url in url_lists]
             for future in as_completed(futures):
                 category_item.extend(future.result())
 
-        return category_item 
+        return category_item
