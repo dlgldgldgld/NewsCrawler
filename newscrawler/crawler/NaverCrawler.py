819132cde4baa07f4b977dd125e066b1e5cebd0f
dlgldgld <shin12272014@gmail.com>
2022-06-03 14:42:21 +0900

    update - navernews classid update..

--- newscrawler/crawler/NaverCrawler.py
+++ newscrawler/crawler/NaverCrawler.py
@@ -2,14 +2,14 @@ if __name__ == "__main__" :
     import sys
     import os
     sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
     from iCrawler import iCrawler
-    
 else :
-    from newscrawler.crawler.iCrawler import iCrawler
+    import os 
     
-from newscrawler.custom_type.urllist import urllist
-from newscrawler.custom_type.outrecord import outrecord
+    from crawler.iCrawler import iCrawler
+    
+from custom_type.urllist import urllist
+from custom_type.outrecord import outrecord
 
 import time
 
@@ -34,8 +34,9 @@ class NaverCrawler ( iCrawler ) :
         comm_cnt = None
         intrest_cnt = None
         created_time = None
-
-        comments = driver.find_element(By.CSS_SELECTOR, '#articleTitleCommentCount > span.lo_txt')
+        
+        # 기사의 Comment Count 찾기
+        comments = driver.find_element(By.CSS_SELECTOR, '#comment_count')
         time.sleep(0.25)
         if comments.text == '' :
             comm_cnt = None
@@ -47,15 +48,15 @@ class NaverCrawler ( iCrawler ) :
 
         try :
             driver.implicitly_wait(0)
-            like_div  = driver.find_element(By.CSS_SELECTOR, '#main_content > div.article_header > div.article_info > div > div.article_btns > div.article_btns_left > div > a')
-            like_tags = like_div.find_element(By.CSS_SELECTOR , 'span.u_likeit_text._count.num' )
+            # 기사의 반응 갯수 찾기 Count 찾기
+            like_tags = driver.find_element(By.CSS_SELECTOR , '#commentFontGroup > div.media_end_head_info_variety_likeit._LIKE_HIDE.as_likeit_improve > div > a > span.u_likeit_text._count.num' )
         except NoSuchElementException as e :
             pass
         else :
             intrest_cnt = like_tags.text
 
         try : 
-            create_time_tag = driver.find_element(By.CSS_SELECTOR, '#main_content > div.article_header > div.article_info > div > span.t11')
+            create_time_tag = driver.find_element(By.CSS_SELECTOR, '#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span')
         except NoSuchElementException as e :
             pass
         else :
@@ -78,14 +79,16 @@ class NaverCrawler ( iCrawler ) :
 
 if __name__ == "__main__" :
     # initialize Class 
-    test = NaverCrawler(urlpath = [urllist('Politic', 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')])
-
+    test = NaverCrawler()
+    test.urlpath = [urllist('Politic', 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')]
     # test 1 : getNewsItems test
-    items = test.Exec()
+    
+    items = test.getNewsItem(test.urlpath[0])
     for i in items :
         print (i.getRecByDict())
 
     # test 2 : getAddContent test
     item = outrecord('hi','hi', 'https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=105&oid=421&aid=0005880721')
-    item.comm_cnt, item.interest_cnt, item.created_time = test.getAddContent('https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=105&oid=421&aid=0005880721')
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    item.comm_cnt, item.interest_cnt, item.created_time = test.getAddContent(driver, 'https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=105&oid=421&aid=0005880721')
     print(item.getRecByDict())
