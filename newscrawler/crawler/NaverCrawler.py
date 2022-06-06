9aad3f0469cb2ec015c7d9ce6dbff2026b9f8b57
dlgldgld <shin12272014@gmail.com>
2022-06-07 02:29:17 +0900

    it will be executed in linux and window

--- newscrawler/crawler/NaverCrawler.py
+++ newscrawler/crawler/NaverCrawler.py
@@ -18,8 +18,6 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import *
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -36,31 +34,23 @@ class NaverCrawler(iCrawler):
         
         # 기사의 Comment Count 찾기
         try :
-            comments_tag = WebDriverWait(driver, 3).until(
-                    ec.presence_of_element_located(
-                        (By.CSS_SELECTOR, '#comment_count'))
-                    )
-        except NoSuchElementException as e:
+            comments = driver.find_element(By.CSS_SELECTOR, '#comment_count')
+            time.sleep(0.25)
+        except NoSuchElementException as e :
             pass
         else :
-            comm_cnt = '0' if not comments_tag.text.isdigit() else comments_tag.text
+            comm_cnt = '0' if not comments.text.isdigit() else comments.text
 
         try:
             # 기사의 반응 갯수 찾기 Count 찾기
-            like_tags = WebDriverWait(driver, 3).until(
-                ec.presence_of_element_located(
-                    (By.CSS_SELECTOR, "#commentFontGroup > div.media_end_head_info_variety_likeit._LIKE_HIDE.as_likeit_improve > div > a > span.u_likeit_text._count"))
-                )
+            like_tags = driver.find_element(By.CSS_SELECTOR , '#commentFontGroup > div.media_end_head_info_variety_likeit._LIKE_HIDE.as_likeit_improve > div > a > span.u_likeit_text._count.num' )
         except NoSuchElementException as e :
             pass
         else:
             intrest_cnt = like_tags.text
 
         try : 
-            create_time_tag = WebDriverWait(driver, 3).until(
-                ec.presence_of_element_located(
-                    (By.CSS_SELECTOR, '#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span'))
-                )
+            create_time_tag = driver.find_element(By.CSS_SELECTOR, '#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span')
         except NoSuchElementException as e :
             pass
         else:
@@ -69,7 +59,12 @@ class NaverCrawler(iCrawler):
         return comm_cnt, intrest_cnt, created_time
 
     def getnewsitem(self, url) -> list:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options = options)
+        driver.implicitly_wait(0.5)
         html = urlopen(url.url)
         bsObj = BeautifulSoup(html.read(), "html.parser")
         headline = bsObj.findAll("a", {"class": "cluster_text_headline"})
